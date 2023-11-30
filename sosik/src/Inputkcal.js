0ac72fc85e48026fbf56729ae7d138705b724bc2
hyeonji <hyunji11092@gmail.com>
2023-11-30 18:17:51 +0900

    section3 작업
    1차 작업 완료

---
+++ sosik/src/Inputkcal.js
@@ -0,0 +1,13 @@
+import Inputkcalcard from './component/inputkcal/Inputkcalcard.js';
+import Inputkcalimg from './component/inputkcal/Inputkcalimg.js';
+
+function Inputkcal() {
+  return (
+    <div>
+      <Inputkcalimg/>
+      <Inputkcalcard/>
+    </div>
+  );
+}
+
+export default Inputkcal;
