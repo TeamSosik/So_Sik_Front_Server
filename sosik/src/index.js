ce68dae8cb02e65236fddfe5b9f8ef0fe5d137e4
ComNewbie <iforking3@naver.com>
2023-12-01 10:38:38 +0900

    Section1 작업
    메인 페이지에  Section1부분을 만들었습니다.

--- sosik/src/index.js
+++ sosik/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import MyHeader from './component/header/MyHeader';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import MyHeader from "./component/header/MyHeader";
+import Section1 from "./component/section1/section1";
+import Section2 from "./component/"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <MyHeader></MyHeader>
+    <Section1></Section1>
+    <Section2></Section2>
+    <Section3></Section3>
+    <Footer></Footer>
   </React.StrictMode>
 );
 
