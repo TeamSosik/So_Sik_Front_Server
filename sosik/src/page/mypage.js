8e6001ed640341589b4fbd80ef643f589697e006
ComNewbie <iforking3@naver.com>
2023-12-04 19:53:17 +0900

    마이페이지  수정 및 병합
    마이페이지 부분 헤더와 호진님(Section1),다혜님(Section2) ,경민님(Section3)병합하였습니다.

--- sosik/src/page/mypage.js
+++ sosik/src/page/mypage.js
@@ -2,13 +2,16 @@ import React from "react";
 import Section3 from "../component/mypage_section3/mypage_section3";
 import Section1 from "../component/lhj/mypage/Section1";
 import MyHeader from "../component/header/MyHeader";
+import "../common/css/mypage_section3/myfeed.css";
 
 function Mypage() {
   return (
     <div>
       <MyHeader></MyHeader>
       <Section1></Section1>
-      <Section3></Section3>
+      <div className="section3">
+        <Section3></Section3>
+      </div>
     </div>
   );
 }
