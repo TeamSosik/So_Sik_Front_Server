7ff0f79f9956ee3207714965a00ce14b16c9a0b4
ComNewbie <iforking3@naver.com>
2024-01-09 09:34:47 +0900

    #feat 회원정보수정 백엔드와 연결
    ## 🏷 작업 분류
    
    - []  기능 추가
    - [x]  리팩토링
    - [ ]  버그 수정
    - [ ]  환경 수정 및 기타
    
    ## 💡 작업 개요
    회원정보 업데이트 화면을 마이페이지와 연결하고 업데이트하면 백엔드로 넘어갈 수 있도록 연결해 주었습니다
    ## ***📜** 리뷰 요청사항*

--- sosik/src/component/header/Header.js
+++ sosik/src/component/header/Header.js
@@ -3,29 +3,27 @@ import logo from "../../images/logo.png";
 import "../../common/css/header/header.css";
 
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
-import Recipeboardlist from "../../page/recipeboardlist.js";
+import Recipeboardlist from "../../page/Recipeboardlist.js";
 import Feed from "../Feed/FeedContainer";
 import Mainpage from "../../page/MainPage";
 import Login from "../../component/Login.js";
 import FoodSearch from "../foodSearch/FoodSearch";
 import Signup from "../Signup";
 import Mypage from "../../component/Mypage.js";
-import axios from 'axios';
-
+import axios from "axios";
+import UpdateInfo from "../updatemyinfo/UpdateInfo.js";
 
 export const HeaderContext = createContext();
 
 const Header = () => {
   const [logout, setlogout] = useState(true);
-  
+
   useEffect(() => {
     const access = window.localStorage.getItem("accesstoken");
-    if(access === null){
-      setlogout(true); 
-      
-    }else{
+    if (access === null) {
+      setlogout(true);
+    } else {
       setlogout(false);
-      
     }
   }, [logout]);
 
@@ -33,100 +31,122 @@ const Header = () => {
     try {
       console.log("들어왓어요");
       // const response = await axios('http://localhost:9000/members/logout');
-      const accesstoken = JSON.parse(window.localStorage.getItem("accesstoken"));
-      const refreshtoken = JSON.parse(window.localStorage.getItem("refreshtoken"));
+      const accesstoken = JSON.parse(
+        window.localStorage.getItem("accesstoken")
+      );
+      const refreshtoken = JSON.parse(
+        window.localStorage.getItem("refreshtoken")
+      );
       const member = JSON.parse(window.localStorage.getItem("member"));
 
       console.log(accesstoken);
       console.log(refreshtoken);
       console.log(member.email);
 
-    await axios({
-      method: 'post',
-      url: '/members/logout',
-      baseURL: 'http://localhost:5056',
-      headers: {
-          "authorization": "Bearer " + accesstoken,
-          "refreshToken": "Bearer " + refreshtoken, 
-          "member": member.email,
-          "Content-Type": "application/json"
-      },
-      data: JSON.stringify({email:member.email})
-      
-    });
+      await axios({
+        method: "post",
+        url: "/members/logout",
+        baseURL: "http://localhost:5056",
+        headers: {
+          authorization: "Bearer " + accesstoken,
+          refreshToken: "Bearer " + refreshtoken,
+          member: member.email,
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({ email: member.email }),
+      });
 
-    console.log("성공");
-    window.localStorage.removeItem("accesstoken");
-    window.localStorage.removeItem("refreshtoken");
-    window.localStorage.removeItem("member");
+      console.log("성공");
+      window.localStorage.removeItem("accesstoken");
+      window.localStorage.removeItem("refreshtoken");
+      window.localStorage.removeItem("member");
 
-    // console.log(response);
-    setlogout(true);
-    alert("로그아웃 되었습니다.");
-
-     
+      // console.log(response);
+      setlogout(true);
+      alert("로그아웃 되었습니다.");
     } catch (error) {
-      console.error('로그아웃 오류:', error);
+      console.error("로그아웃 오류:", error);
     }
   };
 
-  let loginview  = ""
+  let loginview = "";
 
   if (logout === false) {
     loginview = (
       <>
-        <li><Link to="/mypage" style={{ marginRight: '30px' }}>마이페이지</Link>
-        <Link to="/mainpage" onClick={handleLogout}>로그아웃</Link></li>
+        <li>
+          <Link to="/mypage" style={{ marginRight: "30px" }}>
+            마이페이지
+          </Link>
+          <Link to="/mainpage" onClick={handleLogout}>
+            로그아웃
+          </Link>
+        </li>
       </>
     );
   } else {
-    loginview = <li><Link to="/login">로그인</Link></li>;
+    loginview = (
+      <li>
+        <Link to="/login">로그인</Link>
+      </li>
+    );
   }
-  
+
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <header id="header" className="nav-down">
         {/* <a className="logo" href="/"> */}
         <Link to="/mainpage" className="logo">
-        <img src={logo} alt="Logo" /> 
+          <img src={logo} alt="Logo" />
         </Link>
         {/* </a> */}
         <nav id="gnb">
           <ul className="depth-1">
-            <li><Link to="/foodsearch">칼로리</Link>
-              </li>
-              {/* <li><a href="">랭킹</a>
+            <li>
+              <Link to="/foodsearch">칼로리</Link>
+            </li>
+            {/* <li><a href="">랭킹</a>
                 <ul className="depth-2">
                   <li><a href="">레시피</a>
                   </li></ul>
               </li> */}
-              <li><Link to="/feed">SNS</Link>
-              </li>
-              <li><Link to="/recipeboardlist">커뮤니티</Link>
-                <ul className="depth-2">
-                <li><Link to="/recipeboardlist">요리해요</Link></li>
-                <li><a href="">고민있어요</a>
-                </li><li><a href="">성공했어요</a>
+            <li>
+              <Link to="/feed">SNS</Link>
+            </li>
+            <li>
+              <Link to="/recipeboardlist">커뮤니티</Link>
+              <ul className="depth-2">
+                <li>
+                  <Link to="/recipeboardlist">요리해요</Link>
                 </li>
-                </ul>
-              </li>
+                <li>
+                  <a href="">고민있어요</a>
+                </li>
+                <li>
+                  <a href="">성공했어요</a>
+                </li>
+              </ul>
+            </li>
           </ul>
         </nav>
         <div className="utWrap">
-          <ul className="logWrap">
-          {loginview}
-          </ul>
+          <ul className="logWrap">{loginview}</ul>
           <div className="searchWrap">
             <input type="text" placeholder="어떤 요리가 궁금하신가요?" />
-            <button className="" type="button" id="topSearchBtn">검색</button>
+            <button className="" type="button" id="topSearchBtn">
+              검색
+            </button>
           </div>
           {/* <div class="searchWrap">
             <input type="text" name="q"/>
               <button id="topSearchBtn" class="" type="button" onclick="topSearch()">검색</button>
           </div> */}
 
-          <button type="button" class="ham" onclick="$(this).toggleClass('cross')">
+          <button
+            type="button"
+            class="ham"
+            onclick="$(this).toggleClass('cross')"
+          >
             <div class="bar"></div>
             <div class="bar"></div>
             <div class="bar"></div>
@@ -137,18 +157,19 @@ const Header = () => {
               <Link to="/mypage">마이페이지</Link>
         </div> */}
       </header>
-        <HeaderContext.Provider value={{setlogout}}>
-          <Routes>
-            <Route path="/recipeboardlist" element={<Recipeboardlist />} />
-            <Route path="/feed" element={<Feed/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/mainpage" element={<Mainpage/>}/>
-            <Route path="/foodsearch" element={<FoodSearch/>}/>
-            <Route path="/signup" element={<Signup/>} />
-            <Route path="/mypage" element={<Mypage/>} />
-          </Routes>
-        </HeaderContext.Provider>
-      </BrowserRouter>
+      <HeaderContext.Provider value={{ setlogout }}>
+        <Routes>
+          <Route path="/recipeboardlist" element={<Recipeboardlist />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mainpage" element={<Mainpage />} />
+          <Route path="/foodsearch" element={<FoodSearch />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/updateinfo" element={<UpdateInfo />} />
+        </Routes>
+      </HeaderContext.Provider>
+    </BrowserRouter>
   );
 };
 
