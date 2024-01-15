8c8900d0c9f7a4494c70ee9ab7c3ee24f0d6ddb3
dahye <oda0103@naver.com>
2024-01-15 11:37:21 +0900

    Fix : ' 나의 체중 수정 ' 버그 수정
    마이페이지에서 나의 체중 수정을 눌렀을 때 모달 창이 나오지 않는 버그를 수정하였습니다.

--- sosik/src/component/member/mypage/Mypage.js
+++ sosik/src/component/member/mypage/Mypage.js
@@ -1,35 +1,18 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import WeightModal from "../../intake/modal/WeightModal";
 import "./mypage.css";
 import Aside from "./aside/Aside";
 import MyInfo from "./content/MyInfo";
 import Graph from "./content/Graph";
 
 const MyPage = () => {
-  const navigate = useNavigate();
-  const [showModal, setShowModal] = useState(false);
-  const userInfo =JSON.parse(window.localStorage.getItem("member"));  
- 
-  
-  const handleNavigate = (path) => {
-    navigate(path);
-  };
 
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  
   return (
     <div className="my-page">
-      <Aside/>
+      <Aside />
       <div className="right-section">
-        <MyInfo/>
+        <MyInfo />
         <Graph />
       </div>
-      {showModal && <WeightModal handleCloseModal={() => setShowModal(false)} />}
     </div>
   );
 };
