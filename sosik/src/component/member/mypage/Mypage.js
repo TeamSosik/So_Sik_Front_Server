b7170df9e8a0477a8f88f53653742180a3a51a7f
ComNewbie <iforking3@naver.com>
2024-01-16 14:05:55 +0900

    #refactor Data를 Axios로 가져오게 수정
    ## 🏷 작업 분류
    
    - [ ]  기능 추가
    - [x]  리팩토링
    - [ ]  버그 수정
    - [ ]  환경 수정 및 기타
    
    ## 💡 작업 개요
    회원정보를 로컬스토리지가 아닌 Axios로 가져오게 수정하였습니다!
    ## ***📜** 리뷰 요청사항*

--- sosik/src/component/member/mypage/Mypage.js
+++ sosik/src/component/member/mypage/Mypage.js
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./mypage.css";
 import Aside from "./aside/Aside";
 import MyInfo from "./content/MyInfo";
 import Graph from "./content/Graph";
+import axios from "axios";
 
 const MyPage = () => {
+  const [users, setUsers] = useState({
+    memberId: "",
+    email: "",
+    name: "",
+    gender: "",
+    height: 0,
+    role: "",
+    activityLevel: "",
+    nickname: "",
+    profileImage: "",
+    birthday: "",
+    tdeeCalculation: "",
+    weightList: [""],
+  });
+  const getMemberDetail = async () => {
+    const authorization = JSON.parse(localStorage.getItem("accesstoken"));
+    const refreshToken = JSON.parse(localStorage.getItem("refreshtoken"));
 
+    try {
+      await axios({
+        method: "get",
+        url: "http://localhost:5056/members/v1/detail",
+        headers: {
+          authorization: authorization,
+          refreshToken: refreshToken,
+          "Content-Type": "application/json",
+        },
+      }).then((response) => {
+        console.log(response);
+        console.log(response.data.result);
+        setUsers(response.data.result);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getMemberDetail();
+  }, []);
   return (
     <div className="my-page">
-      <Aside />
+      <Aside props={users} />
       <div className="right-section">
-        <MyInfo />
-        <Graph />
+        <MyInfo props={users} />
+        <Graph props={users} />
       </div>
     </div>
   );
--- sosik/src/component/member/mypage/aside/Aside.js
+++ sosik/src/component/member/mypage/aside/Aside.js
@@ -1,14 +1,11 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./aside.css";
 import WeightModal from "../../../intake/modal/WeightModal";
 
-const Aside = () => {
-
-  const userInfo = JSON.parse(window.localStorage.getItem("member"));
-
+const Aside = ({ props }) => {
   const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
 
@@ -24,23 +21,68 @@ const Aside = () => {
     <>
       <div className="left-section">
         <div className="profile-info">
-          <img src={`http://localhost:5056/members/v1/images/${userInfo.result.memberId}`} alt="" />
-          <h2>{userInfo.result.nickname} 님</h2>
+          <img
+            src={`http://localhost:5056/members/v1/images/${props.memberId}`}
+            alt=""
+          />
+          <h2>{props.nickname} 님</h2>
         </div>
         <div className="update-btn">
-          <button className="my-kcal" type="submit" onClick={() => handleNavigate('/recdkcal')}>나의 칼로리<FontAwesomeIcon icon={faAngleRight} size="2xs" style={{ color: "#000000", marginLeft: 30 }} /></button>
-          <button className="my-anly" type="submit" onClick={() => handleNavigate('/recdanly')}>나의 분석<FontAwesomeIcon icon={faAngleRight} size="2xs" style={{ color: "#000000", marginLeft: 30 }} /></button>
-          <button className="myweight-update" type="submit" onClick={handleShowModal}>
-            나의 체중 수정<FontAwesomeIcon icon={faAngleRight} size="2xs" style={{ color: "#000000", marginLeft: 30 }} />
+          <button
+            className="my-kcal"
+            type="submit"
+            onClick={() => handleNavigate("/recdkcal")}
+          >
+            나의 칼로리
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              size="2xs"
+              style={{ color: "#000000", marginLeft: 30 }}
+            />
           </button>
-          <button className="myinfo-update" type="submit" onClick={() => handleNavigate('/updateinfo')}>내 정보 수정<FontAwesomeIcon icon={faAngleRight} size="2xs" style={{ color: "#000000", marginLeft: 30 }} /></button>
+          <button
+            className="my-anly"
+            type="submit"
+            onClick={() => handleNavigate("/recdanly")}
+          >
+            나의 분석
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              size="2xs"
+              style={{ color: "#000000", marginLeft: 30 }}
+            />
+          </button>
+          <button
+            className="myweight-update"
+            type="submit"
+            onClick={handleShowModal}
+          >
+            나의 체중 수정
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              size="2xs"
+              style={{ color: "#000000", marginLeft: 30 }}
+            />
+          </button>
+          <button
+            className="myinfo-update"
+            type="submit"
+            onClick={() => handleNavigate("/updateinfo")}
+          >
+            내 정보 수정
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              size="2xs"
+              style={{ color: "#000000", marginLeft: 30 }}
+            />
+          </button>
         </div>
       </div>
-      {showModal && <WeightModal handleCloseModal={() => setShowModal(false)} />}
+      {showModal && (
+        <WeightModal handleCloseModal={() => setShowModal(false)} />
+      )}
     </>
   );
 };
 
 export default Aside;
-
-
