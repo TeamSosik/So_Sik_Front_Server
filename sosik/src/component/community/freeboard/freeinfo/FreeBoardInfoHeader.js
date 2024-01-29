2ca624fa878a089eee9f8427a1cdbf9919ad2d91
ComNewbie <iforking3@naver.com>
2024-01-29 17:17:52 +0900

    feat 게시글 불러올때 닉네임 불러오기
    ## 🏷 작업 분류
    
    - [x]  기능 추가
    - [ ]  리팩토링
    - [ ]  버그 수정
    - [ ]  환경 수정 및 기타
    
    ## 💡 작업 개요
    닉네임 불러오는 path 게이트웨이에서 삭제했습니다!
    ## ***📜** 리뷰 요청사항*

--- sosik/src/component/community/freeboard/freeinfo/FreeBoardInfoHeader.js
+++ sosik/src/component/community/freeboard/freeinfo/FreeBoardInfoHeader.js
@@ -2,37 +2,23 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./freeboardinfoheader.css";
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-function FreeBoardInfoHeader({ title, memberId, date }) {
+import axios from "axios";
+function FreeBoardInfoHeader({ title, nickname, date }) {
   const createdAtDate = new Date(date);
-  const formattedDate = `${createdAtDate.getFullYear()}-${(createdAtDate.getMonth() + 1).toString().padStart(2, '0')}-${createdAtDate.getDate().toString().padStart(2, '0')} ${createdAtDate.getHours().toString().padStart(2, '0')}:${createdAtDate.getMinutes().toString().padStart(2, '0')}`;
-
-  const [nickname, setNickname] = useState([]);
-
-  const getData = async () => {
-      const authorization = JSON.parse(sessionStorage.getItem("accesstoken"));
-      const refreshToken = JSON.parse(sessionStorage.getItem("refreshtoken"));
-
-      try {
-        const response = await axios({
-          method: "get",
-          url: 'http://127.0.0.1:5056/members/v1/' + memberId,
-          headers: {
-            authorization: authorization,
-            refreshToken: refreshToken,
-          },
-        }).then((response) => {
-          const resultData = response.data;
-          setNickname(resultData);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    useEffect(() => {
-      getData();
-    }, []);
+  const formattedDate = `${createdAtDate.getFullYear()}-${(
+    createdAtDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}-${createdAtDate
+    .getDate()
+    .toString()
+    .padStart(2, "0")} ${createdAtDate
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${createdAtDate
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
 
   return (
     <Row className="recipeheaderesult">
@@ -44,7 +30,6 @@ function FreeBoardInfoHeader({ title, memberId, date }) {
             <span className="recipeinfoname">{nickname}</span> |{" "}
             <span className="recipeinfodate">{formattedDate}</span>
           </div>
-          
         </div>
         <hr />
       </Col>
