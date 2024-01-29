d4f3b26d51d7ffafb64e560c4960d4b75d3aaa5e
ComNewbie <iforking3@naver.com>
2024-01-29 12:39:22 +0900

    fix: 로그인 디자인 수정
    ## 🏷 작업 분류
    
    - [ ]  기능 추가
    - [ ]  리팩토링
    - [x]  버그 수정
    - [ ]  환경 수정 및 기타
    
    ## 💡 작업 개요
    로그인폼 footer 위치 안맞던거 수정했습니다
    ## ***📜** 리뷰 요청사항*

--- sosik/src/component/member/loginform/Login.js
+++ sosik/src/component/member/loginform/Login.js
@@ -78,7 +78,6 @@ function Login() {
       <Row>
         <Col></Col>
         <Col xs={3} className="logincontainer">
-          <h1>회원가입</h1>
           <Form onSubmit={handleLogin}>
             <Form.Group as={Col}>
               <Form.Label className="loginfont">이메일</Form.Label>
