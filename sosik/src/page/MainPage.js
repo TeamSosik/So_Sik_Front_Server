6c27b4c51eb5fe1f67c4a83e5111c9a7136be475
dahye <oda0103@naver.com>
2024-02-06 09:46:58 +0900

    style : 메인 페이지 이미지 수정
    메인 페이지의 이미지를 수정하였습니다.

--- sosik/src/page/MainPage.js
+++ sosik/src/page/MainPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './mainpage.css'
+import foodsearch from "../images/foodsearch.png";
+import community from "../images/community.png";
+// <a href="https://kr.freepik.com/free-vector/group-of-young-people-posing-for-a-photo_5230711.htm#&position=35&from_view=author&uuid=bab30dab-0dcf-472e-9298-b734980f2c32">작가 pikisuperstar</a> 출처 Freepik
 
 function Mainpage() {
     const member = JSON.parse(window.sessionStorage.getItem("member"));
@@ -12,13 +15,9 @@ function Mainpage() {
                     <Link to={!member || !member.result ? "/login" : "/recdkcal"}>
                         <div data-v-4b6b04e7="" className="box_responsible type_esg">
                             <span data-v-4b6b04e7="" className="title_responsible" />
-                            <img data-v-a968263a="" data-v-4b6b04e7="" src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/service/main/esg.gif" className="img_responsible type_esg" />
-                            <p data-v-4b6b04e7="" className="text_responsible">{
-                            !member || !member.result
-                                ? "로그인"
-                                : "식단관리"
-                            }</p>
-                            
+                            <img data-v-a968263a="" data-v-4b6b04e7="" src={foodsearch} className="img_foodsearch" />
+                            <p data-v-4b6b04e7="" className="text_responsible">식단 관리</p>
+
                             <a data-v-4b6b04e7="" href="/page/responsible/esg" data-tiara-layer="Area_Main_responsible" data-tiara-action-name="Click_Main_esg" className="btn_area">
                                 <span data-v-4b6b04e7="" className="screen_out">바로가기</span>
                                 <svg data-v-42492cb5="" data-v-4b6b04e7="" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none" className="btn_arr_big">
@@ -32,7 +31,7 @@ function Mainpage() {
                     <Link to="/freeboard">
                         <div data-v-4b6b04e7="" className="box_responsible type_green">
                             <span data-v-4b6b04e7="" className="title_responsible" />
-                            <img data-v-a968263a="" data-v-4b6b04e7="" src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/service/main/green.gif" className="img_responsible" />
+                            <img data-v-a968263a="" data-v-4b6b04e7="" src={community} className="img_community" />
                             <p data-v-4b6b04e7="" className="text_responsible">커뮤니티</p>
                             <a data-v-4b6b04e7="" href="/page/responsible/activegreen" data-tiara-layer="Area_Main_responsible" data-tiara-action-name="Click_Main_activeGreen" className="btn_area">
                                 <svg data-v-42492cb5="" data-v-4b6b04e7="" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none" className="btn_arr_big">
